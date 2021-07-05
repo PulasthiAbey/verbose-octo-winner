2039292b0f2abfacdb90ac07fb47388d76feef7a
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2021-07-06 00:32:22 +0530

    Changes Commited

---
+++ Journal/App.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class App extends component {
+ render() {
+   return (
+     <View style={styles.container}>
+       <Text>Hello!</Text>
+     </View>
+   );
+ }
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default StackNavigator({
+  Home: {
+    screen: App 
+  }
+})
